2642d21891c5f4795f45f628bd09e61afa1f8f10
Zack <lorenx@163.com>
2023-08-18 17:04:53 +0800

    feat: update text

--- src/client/components/features-02.tsx
+++ src/client/components/features-02.tsx
@@ -22,7 +22,7 @@ export default function Features02() {
           { /* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4">Faster. Smarter.</h2>
-            <p className="text-lg text-slate-400">There are many variations available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            <p className="text-lg text-slate-400">{ "There are many variations available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable." }</p>
           </div>
 
           { /* Highlighted boxes */}
